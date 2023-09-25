918110de092c4420140f705f42de89d3066d9608
farida eryani <54765115+matahariku@users.noreply.github.com>
2023-09-25 15:43:18 +0200

    Update and rename test_my_module.py.txt to test_apply.py

---
+++ test/test_apply.py
@@ -0,0 +1,12 @@
+import unittest
+from src.app import app
+
+class TestApp(unittest.TestCase):
+
+    def setUp(self):
+        self.app = app.test_client()
+
+    def test_index(self):
+        response = self.app.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data.decode('utf-8'), "Hello, world!")
